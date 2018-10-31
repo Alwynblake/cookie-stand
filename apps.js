9d89463e1f27d0ce8c57c164bda40e7072ba9bf9
Alistair Blake <consultations@me.com>
2018-10-30 18:05:05 -0700

    lab7

--- apps.js
+++ apps.js
@@ -1,3 +1,9 @@
+
+//a variable that's an array of objects
+//objects are: location, min hourly customers, max hourly customers, average cookies sold
+//random number generator (hour many customers showed ber hour)-function in the object, 
+//total cookies sold.
+
 //define an object with a store
 var objectlipikeAndFirst = {
   storename: 'first and pike',
@@ -7,7 +13,7 @@ var objectlipikeAndFirst = {
   custPerHour: [],//generate a random number of customers
   cookiesPerHour: [],
   //each store should have varying hours of operation ==> use a for loop to do this.
-  hoursOfOps: ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'],
+  hoursOfOps: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm'],
   dailyTotal: 0,
   generateRandomCustPerHour: function (min, max) {
     for (var i = 0; i < this.hoursOfOps.length; i++) {
@@ -24,34 +30,122 @@ var objectlipikeAndFirst = {
       this.cookiesPerHour.push(perHour);
 
       //this.dailyTotal = this.dailyTotal + perHour
-
-      this.dailyTotal += perHour;
     }
+    this.dailyTotal = this.dailyTotal + perHour;
   },
+
   render: function () {
     //line below will generate hourly sales which also generates customers per hour
     this.generateGourlySales();
 
-    var mainEl = document.getElementById('main-content');
-    var containerEl = document.createElement('section');
-
-    var headingEl = document.createElement('h3');
-    headingEl.textContent = this.name;
-    containerEl.appendChild(headingEl);
-
-    var ulEl = document.createElement('ul');
-
-    for (var i = 0; i < this.hoursOfOps.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${this.hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`; // '6am: 23 cookies'
-      ulEl.appendChild(liEl);
-    }
-
-    var totalEl = document.createElement('li');
-    totalEl.textContent = `Total: ${this.dailyTotal} cookies`;
-    ulEl.appendChild(totalEl);
-
-    containerEl.appendChild(ulEl);
-    mainEl.appendChild(containerEl);
   },
 };
+//create an empty container to hold each store information:
+
+var storename = [];
+
+this.minCustPerHour =
+  this.maxCustPerHour =
+  this.avgCookiesPerCust =
+  this.custPerHour =
+  this.cookiesPerHour =
+
+//create a row:
+var trEL = document.createElement('tr');
+var thEL = document.createElement('th');
+thEL.textContent = this.name
+trEL.appendChild(thEl);
+// now use a for loop for cookies per hour:
+
+
+
+//Anchor to the html doc:
+Turrle.prototype.render = function () {
+  var mainEL = documemt.getElementById('main-content');
+  var tblnEL = documemt.getElementById('table');
+  var theadEL = documemt.getElementById('thead');
+  var tbodyEL = documemt.getElementById('tbody');
+  var tfootEL = documemt.getElementById('tfoot');
+
+  mainEL.appendChild(tblEL);
+  tblEL.appendChild(theadEL);
+  tblEL.appendChild(tbodyEL);
+  tblEL.appendChild(tfootEL);
+
+  tblEL.id = 'storename-table';
+  theadEL.id = 'tbl-body';
+  tbodyEL.id = 'tbl-body';
+
+
+}
+
+
+
+
+
+
+
+
+
+
+///////
+// var name = 'Michaelangelo'; // Globally scoped
+
+// function sayHello(name) { // Globally scoped
+//   return `Hello, ${name}, how are you today?`;
+// }
+
+// var turtleMichealangelo = {  // Unordered collections of key: value pairs
+//   // property (key): value,
+//   name: 'Michaelangelo',
+//   shell: true,
+//   age: 15,
+//   color: 'orange',
+//   type: 'box',
+//   size: 62.3,
+//   ninja: true,
+//   mutant: true,
+//   sayHello: function () {
+//     return `Hello, my name is ${this.name}, and I am ${this.size} .`;
+//   },
+// };
+
+// var turtleDonatello = {
+//   // // property: value,
+// name: 'Donatello',
+// shell: true,
+// age: 15,
+// color: 'purple',
+// type: 'box',
+// size: 62.3,
+// ninja: true,
+// mutant: true,
+// sayHello: function () {
+//   return `Hello, my name is Donatello.`;
+// },
+// render: function () {
+  // create an element
+  // provide content for that element
+  // append the element to the page (in a specific place)
+  // var container = document.createElement('bananas');  // This will create <bananas></bananas> BUT DOES NOT WORK
+//   var container = document.createElement('section');
+//   var nameEl = document.createElement('h3');
+//   var messageEl = document.createElement('p');
+
+//   nameEl.textContent = this.name;
+//   messageEl.textContent = `I am a ninja and I am ${this.color}.`;
+
+//   container.appendChild(nameEl);
+//   container.appendChild(messageEl);
+//   console.log('container', container);
+
+//   var mainEl = document.getElementById('main-content');
+//   mainEl.appendChild(container);
+// }
+// };
+
+// var nums = [1, 2, 3, 4];  // arrays should generally have like data types
+
+
+// // call the render function and display the elements
+// turtleDonatello.render();
