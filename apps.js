73a9a149f0f5c51a359dd3855fa64daf648b31ba
Alistair Blake <consultations@me.com>
2018-10-31 17:00:04 -0700

    updates made

--- apps.js
+++ apps.js
@@ -129,3 +129,34 @@ var capitolHill = {
     console.log('cookiesPerHour', this.cookiesPerHour);
   },
 }
+var Alki = {
+  storename: 'Alki',
+  minCustPerHour: 2,
+  maxCustPerHour: 16,
+  avgCookiesPerCust: 4.6,
+  custPerHour: [],//generate a random number of customers
+  cookiesPerHour: [],
+  //each store should have varying hours of operation ==> use a for loop to do this:
+  hoursOfOps: ['6a', '7a', '8a', '9a', '10a', '11a,', '12p', '1p', '2p', '3p', '4p', '5p', '6p', '7p', '8p'],
+  dailyTotal: 0,
+  generateRandomCustPerHour: function () {
+    for (var i = 0; i < this.hoursOfOps.length; i++) {
+      var randomCust = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
+      this.custPerHour.push(randomCust);
+    }
+    console.log('custPerHour', this.custPerHour);
+  },
+  generateHourlySales: function () {
+    //line below will populate custPerHour array
+    this.generateRandomCustPerHour();
+
+    for (var i = 0; i < this.hoursOfOps.length; i++) {
+      var perHour = Math.round(this.custPerHour[i] * this.avgCookiesPerCust);
+      this.cookiesPerHour.push(perHour);
+
+      //this.dailyTotal = this.dailyTotal + perHour
+      this.dailyTotal += perHour;
+    }
+    console.log('cookiesPerHour', this.cookiesPerHour);
+  },
+}
