1e1de3bcbeb198202bf5ac676460da916b26cdc8
Alistair Blake <consultations@me.com>
2018-11-01 23:05:01 -0700

    updates made

--- app.js
+++ app.js
@@ -1,55 +1,50 @@
 'use strict';
 var stores = [];
+
 var hoursOfOps = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
 
-function Store(name, min, max, avg) {
+function Store(name, minCustPerHour, maxCustPerHour, avgCookiesPerCust) {
   this.name = name;
-  this.min = min;
-  this.max = max;
-  this.avg = avg;
+  this.minCustPerHour = minCustPerHour;
+  this.maxCustPerHour = maxCustPerHour;
+  this.avgCookiesPerCust = avgCookiesPerCust;
   this.custPerHour = [];
   this.cookiesPerHour = [];
   this.dailyTotal = 0;
-
   stores.push(this);
   this.render();
-
   createTableFooter();
 }
-
-Store.prototype.generateRandomCustPerHour = function (min, max) {
+Store.prototype.generateRandomCustPerHour = function (minCustPerHour, maxCustPerHour) {
   for (var i = 0; i < hoursOfOps.length; i++) {
-    var randomCust = Math.floor(Math.random() * (max - min + 1) + min);
+    var randomCust = Math.floor(Math.random() * (maxCustPerHour - minCustPerHour
+      + 1) + minCustPerHour
+    );
     this.custPerHour.push(randomCust);
   }
+  console.log('custPerHour', this.custPerHour);
 };
-
 Store.prototype.generateHourlySales = function () {
-
-  this.generateRandomCustPerHour(this.min, this.max);
-
+  //line below will populate custPerHour array
+  this.generateRandomCustPerHour(this.minCustPerHour
+    , this.maxCustPerHour);
   for (var i = 0; i < hoursOfOps.length; i++) {
-    var perHour = Math.round(this.custPerHour[i] * this.avg);
+    var perHour = Math.round(this.custPerHour[i] * this.avgCookiesPerCust);
     this.cookiesPerHour.push(perHour);
-
-
     this.dailyTotal += perHour;
   }
 };
-
 Store.prototype.render = function () {
-
   this.generateHourlySales();
-
+  //step1-DOM: create an element:
   var tbodyEl = document.getElementById('tbl-body');
   var trEl = document.createElement('tr');
-
-
-
   var thEl = document.createElement('th');
+  //step2-DOM: provide content for the element:
   thEl.textContent = this.name;
+  //step3-DOM: append the element to the page (in a specific place):
   trEl.appendChild(thEl);
-
+  //each store should have varying hours of operation ==> use a for loop to do this:
   for (var i = 0; i < hoursOfOps.length; i++) {
     var tdEl = document.createElement('td');
     tdEl.textContent = this.cookiesPerHour[i];
@@ -59,7 +54,6 @@ Store.prototype.render = function () {
   var totalEl = document.createElement('td');
   totalEl.textContent = this.dailyTotal;
   trEl.appendChild(totalEl);
-
   tbodyEl.appendChild(trEl);
 };
 
@@ -75,7 +69,6 @@ function createTableHeader() {
   var theadEl = document.createElement('thead');
   var trEl = document.createElement('tr');
   var emptyTh = document.createElement('th');
-
   trEl.appendChild(emptyTh);
 
   for (var i = 0; i < hoursOfOps.length; i++) {
@@ -109,9 +102,7 @@ function createTableFooter() {
   var tblEl = document.getElementById('sales-table');
   var tfootEl = document.createElement('tfoot');
   var trEl = document.createElement('tr');
-
   tfootEl.id = 'tbl-foot';
-
   var emptyThEl = document.createElement('th');
   trEl.appendChild(emptyThEl);
 
@@ -119,17 +110,15 @@ function createTableFooter() {
   for (var i = 0; i < hoursOfOps.length; i++) {
     var tdEl = document.createElement('td');
     var totals = 0;
-
     for (var j = 0; j < stores.length; j++) {
       totals += stores[j].cookiesPerHour[i];
     }
-
     tdEl.textContent = totals;
     trEl.appendChild(tdEl);
-
     grandTotal += totals;
-  }
+  };
 
+  //Anchor to the html doc:
   var grandTotalEl = document.createElement('td');
   grandTotalEl.textContent = grandTotal;
   trEl.appendChild(grandTotalEl);
@@ -137,16 +126,13 @@ function createTableFooter() {
   tfootEl.appendChild(trEl);
   tblEl.appendChild(tfootEl);
 }
-
-
 (function run() {
   createTable();
   createTableHeader();
   createTableBody();
 })();
 
-
-
+//use 'new' keyword to make the conext local scope:
 new Store('First and Pike', 23, 65, 6.4);
 new Store('SeaTac', 3, 24, 1.2);
 new Store('Seattle Center', 11, 38, 3.7);
